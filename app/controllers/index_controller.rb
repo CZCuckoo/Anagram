761ecdbd583189d405059133b0b947fde4bdefa8
Clint Johnson <Clint.Johnson.07@gmail.com>
2015-12-16 18:17:36 -0500

    uniq working

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,10 +4,11 @@ end
 
 post '/' do
   @word = params[:word]
-  if valid_input?(@word)
+  begin
+    valid_input(@word)
     redirect "/anagrams/#{@word}"
-  else
-    @error = "Your entry is not valid"
+  rescue Exception => error
+    @error = error.message
     erb :index
   end
 end
@@ -18,28 +19,12 @@ get '/anagrams/:word' do
   erb :show
 end
 
-def three_letters?(input)
-  if input.length > 3
-    return false
-  else
-    return true
-  end
-end
-
-def distinct_letters?(input)
+def valid_input(input)
   letter_array = input.chars
-  unique_letters = input.uniq
-  if unique_letters.length < letter_array.length
-    false
-  else
-    true
-  end
-end
-
-def valid_input?(input)
-  if three_letters?(input) && distinct_letters?(input)
-    true
-  else
-    false
+  unique_letters = letter_array.uniq
+  if input.length > 3
+    raise Exception.new("Word must be less than or equal to 3 characters.")
+  elsif unique_letters.length < letter_array.length
+    raise Exception.new("Word must have unique characters")
   end
 end
