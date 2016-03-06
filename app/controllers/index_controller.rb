05da50e94f04dd1a77860a7ffa510d2e5a757dae
Clint Johnson <Clint.Johnson.07@gmail.com>
2016-03-05 19:55:37 -0500

    Attempting to get controller and show.erb to work

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -29,29 +29,3 @@ def valid_input(input)
     raise Exception.new("Word must have unique characters")
   end
 end
-#
-# def three_letters?(input)
-#   if input.length > 3
-#     return false
-#   else
-#     return true
-#   end
-# end
-#
-# def distinct_letters?(input)
-#   letter_array = input.chars
-#   unique_letters = input.uniq
-#   if unique_letters.length < letter_array.length
-#     false
-#   else
-#     true
-#   end
-# end
-#
-# def valid_input?(input)
-#   if three_letters?(input) && distinct_letters?(input)
-#     true
-#   else
-#     false
-#   end
-# end
--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,3 +2,9 @@ get '/words' do
   @words = Word.all
   erb :"/words/index"
 end
+
+get '/words/:id' do
+  label = params[:id]
+  @word = Word.find(label)
+  erb :"/words/show"
+end
